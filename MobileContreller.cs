6822af42e39e749837cbf3cf24da8acedbdd3254
brucerw <74737639+brucerw@users.noreply.github.com>
2020-11-20 01:59:39 +0600

    Add files via upload

---
+++ MobileContreller.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;           //библиотека на интерфейс
+using UnityEngine.EventSystems;//билблиотека на события
+
+public class MobileContreller : MonoBehaviour,IDragHandler,IPointerUpHandler,IPointerDownHandler//виртуальные библиотеки
+{
+    private Image _JoystickBG;
+    [SerializeField]            //проверка в инспекторе
+    private Image _Joystick;
+    private Vector2 inputVector;// получение координатов джойстика
+
+    private void Start()
+    {
+        _JoystickBG = GetComponent<Image>();
+
+       _Joystick = transform.GetChild(0).GetComponent<Image>();
+         
+    }
+    public virtual void OnPointerDown(PointerEventData ped)
+    {
+        OnDrag(ped);
+    }
+    public virtual void OnPointerUp(PointerEventData ped)
+    {
+        inputVector = Vector2.zero;
+
+        _Joystick.rectTransform.anchoredPosition = Vector2.zero;
+    }
+    public virtual void OnDrag(PointerEventData ped)
+    {
+        Vector2 pos;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(_JoystickBG.rectTransform,ped.position,ped.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / _JoystickBG.rectTransform.sizeDelta.x); //полученик координат позиции касания на джойстик
+            pos.y = (pos.y / _JoystickBG.rectTransform.sizeDelta.y); // получение координат позиции касания на джойстик по y
+
+            inputVector = new Vector2(pos.x * 2 - 1, pos.y * 2 - 1); //установка точных координат из касания
+            inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
+
+            _Joystick.rectTransform.anchoredPosition = new Vector2(inputVector.x * (_JoystickBG.rectTransform.sizeDelta.x / 2), inputVector.y * (_JoystickBG.rectTransform.sizeDelta.y / 2));
+             
+        }
+    }
+    public float Horizontal()
+    {
+        if (inputVector.x != 0) return inputVector.x;
+        else return Input.GetAxis("Horizontal");
+
+    
+    }
+    public float Vertical()
+        {
+        if (inputVector.y != 0) return inputVector.y;
+        else return Input.GetAxis("Vertical");
+
+    }
+}
---
+++ charactersMechanics.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charactersMechanics : MonoBehaviour
+{
+    //Параметры
+    public float speedmove;//Скорость персонажа
+    public float jumpPower;//сила прыжка персонажа
+    //Геймплей персонажа
+    private float gravityForce;//гравитация персонажа
+    private Vector3 moveVector;//направелние персонажа
+
+    //ссылки на компоненты
+    private CharacterController ch_controller;
+    private Animator ch_animator;
+    private MobileContreller mContr;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ch_controller = GetComponent<CharacterController>();
+        ch_animator = GetComponent<Animator>();
+        mContr = GameObject.FindGameObjectWithTag("_Joystick").GetComponent<MobileContreller>(); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharactersMove();  //метод движения
+        GamingGravity();  //метод гравитации
+
+
+    }
+    //метод перемещения персонажа
+    private void CharactersMove()
+    {
+        //премещения по поверхности
+        if (ch_controller.isGrounded)
+        {
+            ch_animator.ResetTrigger("Jump");
+            ch_animator.SetBool("Falling", false);
+            moveVector = Vector3.zero;
+            moveVector.x = mContr.Horizontal() * speedmove;
+            moveVector.z = mContr.Vertical() * speedmove;
+
+            //анимация персонажа
+            if (moveVector.x != 0 || moveVector.z != 0) ch_animator.SetBool("Move", true);
+            else ch_animator.SetBool("Move", false);
+
+            //поворот персонажа
+            if (Vector3.Angle(Vector3.forward, moveVector) > 1f || Vector3.Angle(Vector3.forward, moveVector) == 0)
+            {
+                Vector3 direct = Vector3.RotateTowards(transform.forward, moveVector, speedmove, 0.0f);
+                transform.rotation = Quaternion.LookRotation(direct);
+
+            }
+
+        }
+        else
+        {
+            if (gravityForce < -3f) ch_animator.SetBool("Falling", true);
+        }
+       
+
+        moveVector.y = gravityForce;
+        ch_controller.Move(moveVector * Time.deltaTime);//метод передвижения по направлению
+
+    }
+        //метод гравитации
+        private void GamingGravity()
+    {
+        if (!ch_controller.isGrounded) gravityForce -= 20f * Time.deltaTime;
+        else gravityForce = -1f;
+        if (Input.GetKeyDown(KeyCode.Space) && ch_controller.isGrounded)
+        {
+            gravityForce = jumpPower;
+            ch_animator.SetTrigger("Jump");
+        }
+    }
+
+}
